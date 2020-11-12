6530c252c8e526df365654ee505b30bfc34df937
Geovane Fedrecheski <geonnave@gmail.com>
2020-11-12 09:17:34 -0300

    Sign, verify, encode, and decode Sign1 messages

--- lib/coselix.ex
+++ lib/coselix.ex
@@ -3,20 +3,30 @@ defmodule Coselix do
   Documentation for `Coselix`.
   """
 
-  def key_types, do: %{
-    okp: {1, "okp"},
-    symmetric: {4, "symmetric"},
+  @cose_key_types %{
+    okp: 1,
+    symmetric: 4,
   }
+  def key_type(kty) when is_atom(kty), do: @cose_key_types[kty]
+  def key_type(kty) when is_integer(kty), do: invert(@cose_key_types)[kty]
 
-  def curves, do: %{
-    x25519: {4, "x25519"},
-    ed25519: {6, "ed25519"},
+  @cose_curves %{
+    x25519: 4,
+    ed25519: 6,
   }
+  def curve(kty) when is_atom(kty), do: @cose_curves[kty]
+  def curve(kty) when is_integer(kty), do: invert(@cose_curves)[kty]
 
-  def algs, do: %{
-    ecdh_ss_hkdf_256: {-27, "ecdh_ss_hkdf_256"},
-    aes_ccm_16_64_128: {10, "aes_ccm_16_64_128"},
+  @cose_algs %{
+    ecdh_ss_hkdf_256: -27,
+    aes_ccm_16_64_128: 10,
   }
+  def alg(kty) when is_atom(kty), do: @cose_algs[kty]
+  def alg(kty) when is_integer(kty), do: invert(@cose_algs)[kty]
+
+  def invert(a_map) do
+    Enum.map(a_map, fn {key, value} -> {value, key} end) |> Enum.into(%{})
+  end
 
   defmodule Keys.OKP do
     defstruct [:kty, :kid, :alg, :key_ops, :base_iv, :crv, :x, :d]
@@ -24,20 +34,20 @@ defmodule Coselix do
     def generate(:enc) do
       {x, d} = :crypto.generate_key(:ecdh, :x25519)
       %__MODULE__{
-        kty: Coselix.key_types()[:okp],
-        alg: Coselix.algs()[:ecdh_ss_hkdf_256],
-        crv: Coselix.curves()[:x25519],
+        kty: Coselix.key_type(:okp),
+        alg: Coselix.alg(:ecdh_ss_hkdf_256),
+        crv: Coselix.curve(:x25519),
         x: x,
         d: d,
       }
     end
 
     def generate(:sig) do
-      {x, d} = :crypto.generate_key(:ecdh, :x25519) # fix-me: should use ed25519
+      {x, d} = :crypto.generate_key(:eddsa, :ed25519)
       %__MODULE__{
-        kty: Coselix.key_types()[:okp],
-        alg: Coselix.algs()[:ecdh_ss_hkdf_256],
-        crv: Coselix.curves()[:ed25519],
+        kty: Coselix.key_type(:okp),
+        alg: Coselix.alg(:ecdh_ss_hkdf_256),
+        crv: Coselix.curve(:ed25519),
         x: x,
         d: d,
       }
@@ -46,5 +56,52 @@ defmodule Coselix do
 
   defmodule Keys.Symmetric do
     defstruct [:kty, :kid, :alg, :key_ops, :base_iv, :k]
+  end
+
+  defmodule Headers do
+    def encode(header) do
+      header
+    end
+  end
+
+  defmodule Messages.Sign1 do
+    defstruct [:phdr, :uhdr, :payload, :signature]
+
+    def build(payload, phdr \\ %{}, uhdr \\ %{}) do
+      %__MODULE__{phdr: phdr, uhdr: uhdr, payload: payload}
+    end
+
+    def encode(msg, key) do
+      msg = sign(msg, key)
+
+      [
+        Headers.encode(msg.phdr),
+        Headers.encode(msg.uhdr),
+        msg.payload,
+        msg.signature
+      ]
+      |> CBOR.encode()
+    end
+
+    def decode(encoded_msg, key) do
+      {:ok, [phdr, uhdr, payload, signature], _} = CBOR.decode(encoded_msg)
+      msg = %__MODULE__{phdr: phdr, uhdr: uhdr, payload: payload, signature: signature}
+      if verify(msg, key) do
+        msg
+      else
+        false
+      end
+    end
+
+    def sign(msg, key) do
+      %__MODULE__{
+        msg |
+        signature: :crypto.sign(:eddsa, :sha256, msg.payload, [key.d, :ed25519])
+      }
+    end
+
+    def verify(msg, ver_key) do
+      :crypto.verify(:eddsa, :sha256, msg.payload, msg.signature, [ver_key.x, :ed25519])
+    end
   end
 end
--- test/coselix_test.exs
+++ test/coselix_test.exs
@@ -2,7 +2,35 @@ defmodule CoselixTest do
   use ExUnit.Case
   doctest Coselix
 
-  test "greets the world" do
-    IO.inspect Coselix.Keys.OKP.generate(:enc)
+  test "generate okp keys" do
+    assert Coselix.Keys.OKP.generate(:enc).kty == Coselix.key_type(:okp)
+    assert Coselix.Keys.OKP.generate(:enc).crv == Coselix.curve(:x25519)
+    assert Coselix.Keys.OKP.generate(:sig).crv == Coselix.curve(:ed25519)
+  end
+
+  describe "sign1 message" do
+    setup do
+        key = Coselix.Keys.OKP.generate(:sig)
+        msg = Coselix.Messages.Sign1.build("content to sign")
+        {:ok, %{key: key, msg: msg}}
+    end
+
+    test "sign", %{key: key, msg: msg} do
+        msg = Coselix.Messages.Sign1.sign(msg, key)
+        assert Coselix.Messages.Sign1.verify(msg, key)
+
+        # alter signature
+        <<_::binary-size(3)>> <> tmp = msg.signature
+        altered_signature = "aaa" <> tmp
+        altered_msg = Map.put(msg, :signature, altered_signature)
+        refute Coselix.Messages.Sign1.verify(altered_msg, key)
+    end
+
+    test "encode", %{key: key, msg: msg} do
+        msg = Coselix.Messages.Sign1.sign(msg, key)
+        encoded_msg = Coselix.Messages.Sign1.encode(msg, key)
+        decoded_msg = Coselix.Messages.Sign1.decode(encoded_msg, key)
+        assert decoded_msg == msg
+    end
   end
 end
