6efd0ac8f7b67e7b6c61b30770a2e665e3566fb8
Geovane Fedrecheski <geonnave@gmail.com>
2020-11-13 17:53:14 -0300

    COSE Sign1 works (validated against python implementation)

--- lib/coselix.ex
+++ lib/coselix.ex
@@ -14,18 +14,31 @@ defmodule Coselix do
     x25519: 4,
     ed25519: 6,
   }
-  def curve(kty) when is_atom(kty), do: @cose_curves[kty]
-  def curve(kty) when is_integer(kty), do: invert(@cose_curves)[kty]
+  def curve(crv) when is_atom(crv), do: @cose_curves[crv]
+  def curve(crv) when is_integer(crv), do: invert(@cose_curves)[crv]
 
   @cose_algs %{
     ecdh_ss_hkdf_256: -27,
     aes_ccm_16_64_128: 10,
+    eddsa: -8,
   }
-  def alg(kty) when is_atom(kty), do: @cose_algs[kty]
-  def alg(kty) when is_integer(kty), do: invert(@cose_algs)[kty]
+  def algorithm(alg) when is_atom(alg), do: @cose_algs[alg]
+  def algorithm(alg) when is_integer(alg), do: invert(@cose_algs)[alg]
+
+  @cose_headers %{
+    alg: 1,
+    kid: 4,
+    iv: 5,
+  }
+  def header(hdr) when is_atom(hdr), do: @cose_headers[hdr]
+  def header(hdr) when is_integer(hdr), do: invert(@cose_headers)[hdr]
 
   def invert(a_map) do
     Enum.map(a_map, fn {key, value} -> {value, key} end) |> Enum.into(%{})
+  end
+
+  def tag_as_byte(data) when is_binary(data) do
+    %CBOR.Tag{tag: :bytes, value: data}
   end
 
   defmodule Keys.OKP do
@@ -53,9 +66,10 @@ defmodule Coselix do
 
     def sign(to_be_signed, key) do
       :crypto.sign(:eddsa, :sha256, to_be_signed, [key.d, :ed25519])
+      |> Coselix.tag_as_byte()
     end
 
-    def verify(to_be_verified, signature, ver_key) do
+    def verify(to_be_verified, %CBOR.Tag{tag: :bytes, value: signature}, ver_key) do
       :crypto.verify(:eddsa, :sha256, to_be_verified, signature, [ver_key.x, :ed25519])
     end
   end
@@ -65,33 +79,51 @@ defmodule Coselix do
   end
 
   defmodule Headers do
-    def encode_phdr(%{}), do: <<>>
-    def encode_phdr(phdr), do: CBOR.encode(phdr)
-    def decode_phdr(<<>>), do: %{}
-    def decode_phdr(phdr), do: CBOR.decode(phdr)
+    def translate(phdr) do
+      phdr
+      |> Enum.map(fn {k, v} ->
+        cond do
+          k == :alg || k == 1 ->
+            {Coselix.header(k), Coselix.algorithm(v)}
+          true ->
+            {Coselix.header(k), v}
+        end
+      end)
+      |> Enum.into(%{})
+    end
+
+    def tag_phdr(phdr_map) do
+      Coselix.tag_as_byte(CBOR.encode(translate(phdr_map)))
+    end
+
+    def decode_phdr(phdr_bytes) do
+      {:ok, phdr_map, ""} = CBOR.decode(phdr_bytes.value)
+      translate(phdr_map)
+    end
   end
 
   defmodule Messages.Sign1 do
     defstruct [:phdr, :uhdr, :payload, :signature]
 
+    @spec build(binary, map, map) :: map
     def build(payload, phdr \\ %{}, uhdr \\ %{}) do
-      %__MODULE__{phdr: phdr, uhdr: uhdr, payload: payload}
+      %__MODULE__{phdr: phdr, uhdr: uhdr, payload: Coselix.tag_as_byte(payload)}
     end
 
     def encode(msg, key) do
       msg = sign(msg, key)
 
-      [
-        Headers.encode_phdr(msg.phdr),
+      value = [
+        Headers.tag_phdr(msg.phdr),
         msg.uhdr,
         msg.payload,
         msg.signature
       ]
-      |> CBOR.encode()
+      CBOR.encode(%CBOR.Tag{tag: 18, value: value})
     end
 
     def decode(encoded_msg, key) do
-      {:ok, [phdr, uhdr, payload, signature], _} = CBOR.decode(encoded_msg)
+      {:ok, %CBOR.Tag{tag: 18, value: [phdr, uhdr, payload, signature]}, _} = CBOR.decode(encoded_msg)
       msg = %__MODULE__{phdr: Headers.decode_phdr(phdr), uhdr: uhdr, payload: payload, signature: signature}
       if verify(msg, key) do
         msg
@@ -100,14 +132,17 @@ defmodule Coselix do
       end
     end
 
-    def sign(msg, key, external_aad \\ <<>>) do
-      sig_struct = [
+    def sig_structure(msg, _external_aad \\ <<>>) do
+      [
         "Signature1",
-        Headers.encode_phdr(msg.phdr),
-        external_aad,
+        msg.phdr == %{} && <<>> || Headers.tag_phdr(msg.phdr),
+        Coselix.tag_as_byte(<<>>), # _external_aad
         msg.payload
       ]
-      to_be_signed = CBOR.encode(sig_struct)
+    end
+
+    def sign(msg, key, external_aad \\ <<>>) do
+      to_be_signed = CBOR.encode(sig_structure(msg, external_aad))
 
       %__MODULE__{
         msg |
@@ -116,13 +151,7 @@ defmodule Coselix do
     end
 
     def verify(msg, ver_key, external_aad \\ <<>>) do
-      sig_struct = [
-        "Signature1",
-        Headers.encode_phdr(msg.phdr),
-        external_aad,
-        msg.payload
-      ]
-      to_be_verified = CBOR.encode(sig_struct)
+      to_be_verified = CBOR.encode(sig_structure(msg, external_aad))
 
       Coselix.Keys.OKP.verify(to_be_verified, msg.signature, ver_key)
     end
--- test/coselix_test.exs
+++ test/coselix_test.exs
@@ -8,10 +8,27 @@ defmodule CoselixTest do
     assert Coselix.Keys.OKP.generate(:sig).crv == Coselix.curve(:ed25519)
   end
 
+  describe "headers" do
+      test "encode" do
+          assert Coselix.Headers.translate(%{alg: :eddsa}) == %{1 => -8}
+          assert Coselix.Headers.translate(%{alg: :aes_ccm_16_64_128}) == %{1 => 10}
+
+          assert Coselix.Headers.translate(%{1 => -8}) == %{alg: :eddsa}
+
+          assert Coselix.Headers.tag_phdr(%{alg: :eddsa}) == %CBOR.Tag{tag: :bytes, value: <<0xa1, 0x01, 0x27>>}
+
+          phdr = Coselix.Headers.tag_phdr(%{alg: :eddsa})
+          assert Coselix.Headers.decode_phdr(phdr) == %{alg: :eddsa}
+      end
+  end
+
   describe "sign1 message" do
     setup do
         key = Coselix.Keys.OKP.generate(:sig)
-        msg = Coselix.Messages.Sign1.build("content to sign")
+        key = Map.put(key, :d, Base.decode16!("8437C5D1CB4DE744B33B23A943644268A2CC0F11AF66953F74BAB8B395AFCC21"))
+        key = Map.put(key, :x, Base.decode16!("0D89C5C34501D85E9D23EDBFF932AA85B660100C3534D98F8A0722C992D8B324"))
+
+        msg = Coselix.Messages.Sign1.build("content to sign", %{alg: :eddsa})
         {:ok, %{key: key, msg: msg}}
     end
 
@@ -20,17 +37,16 @@ defmodule CoselixTest do
         assert Coselix.Messages.Sign1.verify(msg, key)
 
         # alter signature
-        <<_::binary-size(3)>> <> tmp = msg.signature
+        <<_::binary-size(3)>> <> tmp = msg.signature.value
         altered_signature = "aaa" <> tmp
-        altered_msg = Map.put(msg, :signature, altered_signature)
+        altered_msg = Map.put(msg, :signature, Coselix.tag_as_byte(altered_signature))
         refute Coselix.Messages.Sign1.verify(altered_msg, key)
     end
 
     test "encode", %{key: key, msg: msg} do
-        msg = Coselix.Messages.Sign1.sign(msg, key)
         encoded_msg = Coselix.Messages.Sign1.encode(msg, key)
         decoded_msg = Coselix.Messages.Sign1.decode(encoded_msg, key)
-        assert decoded_msg == msg
+        assert decoded_msg == Coselix.Messages.Sign1.sign(msg, key)
     end
   end
 end
