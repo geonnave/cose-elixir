2fdd69038c8f3a60cb86557d593e6c8f3fdb0897
Geovane Fedrecheski <geonnave@gmail.com>
2021-03-16 12:46:11 -0300

    Encode/Decode CBOR Web Token

---
+++ lib/cose/cwt.ex
@@ -0,0 +1,75 @@
+defmodule COSE.CWT do
+  alias COSE.Messages.{Sign1}
+
+  @spec_claims %{
+    issuer: 1,
+    subject: 2,
+    audience: 3,
+    expiration: 4,
+    not_before: 5,
+    issued_at: 6,
+    token_id: 7
+  }
+
+  def setup_dates(claims) do
+    one_year = 60 * 60 * 24 * 365
+    claims = if claims[:expiration], do: claims, else: Map.put(claims, :expiration, one_year)
+    Map.put(claims, :issued_at, DateTime.to_unix(DateTime.utc_now()))
+  end
+
+  def encode_claim_names(claims, custom_claims \\ %{}) do
+    claims
+    |> Enum.map(fn {name, value} ->
+      key =
+        case @spec_claims[name] do
+          key when is_integer(key) ->
+            key
+
+          _ ->
+            custom_claims[name] || name
+        end
+
+      {key, value}
+    end)
+    |> Enum.into(%{})
+  end
+
+  def encode(claims, key, custom_claims \\ %{}) do
+    claims
+    |> setup_dates()
+    |> encode_claim_names(custom_claims)
+    |> CBOR.encode()
+    |> Sign1.build(%{alg: :eddsa})
+    |> Sign1.encode(key)
+  end
+
+  def decode_claim_names(claims, custom_claims \\ %{}) do
+    claims
+    |> Enum.map(fn {key, value} ->
+      name =
+        case invert_map(@spec_claims)[key] do
+          name when not is_nil(name) ->
+            name
+
+          _ ->
+            invert_map(custom_claims)[key] || key
+        end
+
+      {name, value}
+    end)
+    |> Enum.into(%{})
+  end
+
+  def invert_map(map) do
+    Map.new(map, fn {key, val} -> {val, key} end)
+  end
+
+  def decode(token, key, custom_claims \\ %{}) do
+    if verified_msg = Sign1.decode(token, key) do
+      {:ok, cbor_claims, ""} = CBOR.decode(verified_msg.payload.value)
+
+      cbor_claims
+      |> decode_claim_names(custom_claims)
+    end
+  end
+end
---
+++ test/cwt_test.exs
@@ -0,0 +1,62 @@
+defmodule COSETest.CWT do
+  use ExUnit.Case
+  doctest COSE
+  alias COSE.{Keys, CWT}
+
+  describe "cbor web token" do
+    setup do
+      key = Keys.OKP.generate(:sig)
+
+      d = Base.decode16!("8437C5D1CB4DE744B33B23A943644268A2CC0F11AF66953F74BAB8B395AFCC21")
+      x = Base.decode16!("0D89C5C34501D85E9D23EDBFF932AA85B660100C3534D98F8A0722C992D8B324")
+      key = Map.put(key, :d, d) |> Map.put(:x, x)
+
+      claims = %{
+        expiration: 1_615_898_871,
+        issuer: "did:sw:SpUu2RxLDiz4TeupvSpTaB",
+        subject: "did:sw:6v84ovgu4d9KPvPwHiBJWD"
+      }
+
+      {:ok, %{key: key, claims: claims}}
+    end
+
+    test "encode claims (convert to integer keys)", %{claims: claims} do
+      assert %{4 => 1_615_898_871} = CWT.encode_claim_names(claims)
+
+      claims = Map.put(claims, :custom_claim, "this claim is application-specific")
+      assert %{:custom_claim => _value} = CWT.encode_claim_names(claims)
+
+      custom_claims = %{custom_claim: 22}
+      assert %{22 => _value} = CWT.encode_claim_names(claims, custom_claims)
+    end
+
+    test "decode claims (convert from integer keys)", %{claims: claims} do
+      encoded = CWT.encode_claim_names(claims)
+
+      assert %{:expiration => 1_615_898_871} = CWT.decode_claim_names(encoded)
+
+      custom_claims = %{custom_claim: 22}
+      claims = Map.put(claims, :custom_claim, "this claim is application-specific")
+
+      encoded = CWT.encode_claim_names(claims, custom_claims)
+      assert %{22 => _value} = CWT.decode_claim_names(encoded)
+      assert %{:custom_claim => _value} = CWT.decode_claim_names(encoded, custom_claims)
+    end
+
+    test "encode token", %{key: key, claims: claims} do
+      claims = Map.put(claims, :custom_claim, "this claim is application-specific")
+      custom_claims = %{custom_claim: 22}
+
+      token = CWT.encode(claims, key, custom_claims)
+      assert is_binary(token)
+    end
+
+    test "decode token", %{key: key, claims: claims} do
+      token = CWT.encode(claims, key)
+
+      # remove `issued_at` since original claims do not have it
+      retrived_claims = CWT.decode(token, key) |> Map.delete(:issued_at)
+      assert ^claims = retrived_claims
+    end
+  end
+end
